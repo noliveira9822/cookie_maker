74d90ac763bf7c3b229063f64d7d90060d4cbdde
JohnnyBoy <jaofilipe14>
2021-01-17 13:47:56 +0000

    Adição de funções do turbohostlink->tlnk tab de serial alterada no designer adição de código para operação assincrona funcçao utils.serial_setup e nova classe no utils mainwindow também tem novas funções para serial

--- Utils.py
+++ Utils.py
@@ -1,5 +1,10 @@
 import cv2
+import asyncio
 from PyQt5.QtGui import QImage, QPixmap
+from functools import reduce
+from operator import xor
+from serial.tools.list_ports import comports
+from serial import PARITY_EVEN, PARITY_NONE, PARITY_ODD
 
 
 def img2map(image):
@@ -9,6 +14,16 @@ def img2map(image):
     pixmap = QPixmap.fromImage(qimage)
     return pixmap
 
+def compute_fcs(msg):
+    return format(reduce(xor, map(ord, msg)), 'x')
+
+def HLNK_calculate_frame(node,header,data):
+    message = f'@{node}{header}{data}'
+    fcs = compute_fcs(message)
+    terminator = '*\r'
+    fullmsg = message + fcs + terminator
+    print(fullmsg)
+    return fullmsg
 
 def list_ports():
     '''
@@ -32,3 +47,81 @@ def list_ports():
                 working_ports.append(str(dev_port))
         dev_port += 1
     return working_ports
+
+'''
+Class and setup for serial comms
+'''
+def setup_serial(item):
+    for port in comports(include_links=False):
+        item.combo_comport.addItem(f"{port[0]} {port.description}", port)
+
+    for baud_rate in (9600, 19200, 38400, 57600, 115200):
+        item.combo_baudrate.addItem(str(baud_rate), baud_rate)
+
+    item.combo_data_bits.addItem("7", 7)
+    item.combo_data_bits.addItem("8", 8)
+
+    item.combo_parity.addItem("None", PARITY_NONE)
+    item.combo_parity.addItem("Odd", PARITY_ODD)
+    item.combo_parity.addItem("Even", PARITY_EVEN)
+
+    item.combo_stop_bits.addItem("1", 1)
+    item.combo_stop_bits.addItem("2", 2)
+
+    item.edt_num_plc.setText("00")
+
+
+class Output(asyncio.Protocol):
+    def __init__(self):
+        super(Output, self).__init__()
+        self.recv_callback = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.buf = bytes()
+
+        print('port opened')
+        print(transport)
+
+    def set_recv_callback(self, func):
+        self.recv_callback = func
+
+    def test_comm(self):
+        node = '00'
+        header = 'TS'
+        data = 'TEST'
+        message = f'@{node}{header}{data}'
+        fcs = compute_fcs(message)
+        terminator = '*\r'
+
+        # transport.serial.rts = False  # You can manipulate Serial object via transport
+        self.transport.serial.write(message.encode('ascii'))
+        self.transport.serial.write(fcs.encode('ascii'))
+        self.transport.serial.write(terminator.encode('ascii'))
+
+    def data_received(self, data):
+        """Store characters until a newline is received.
+        """
+        self.buf += data
+        if b'\r' in self.buf:
+            lines = self.buf.split(b'\r')
+            self.buf = lines[-1]  # whatever was left over
+            for line in lines[:-1]:
+                print(f'Reader received: {line.decode()}')
+                self.recv_callback(line.decode())
+
+    def connection_lost(self, exc):
+        print('port closed')
+        self.transport.loop.stop()
+
+    def pause_writing(self):
+        print('pause writing')
+        print(self.transport.get_write_buffer_size())
+
+    def resume_writing(self):
+        print(self.transport.get_write_buffer_size())
+        print('resume writing')
+
+    async def send(self, message):
+        self.transport.serial.write(message.encode('ascii'))
+        print(f'Writer sent: {message}')
--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import sys
 import threading
 import time
+import asyncio
+import traceback
 
 import cv2
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QHBoxLayout, QLineEdit, QComboBox, \
+    QLabel, QPlainTextEdit, QErrorMessage
+
+from qasync import QEventLoop
+from functools import wraps
+
+from serial_asyncio import create_serial_connection
 
 import Utils
+'''
+Coroutines taken from turbohostlink
+'''
+
+def display_error(err):
+    app = QApplication.instance()
+    window = app.activeWindow()
+    dialog = QErrorMessage(window)
+    dialog.setWindowModality(Qt.WindowModal)
+    dialog.setWindowTitle("Error")
+    dialog.showMessage(err)
+
+
+def slot_coroutine(async_func):
+    if not asyncio.iscoroutinefunction(async_func):
+        raise RuntimeError('Must be a coroutine!')
+
+    def log_error(future):
+        try:
+            future.result()
+        except Exception as err:
+            display_error(traceback.format_exc())
+
+    @wraps(async_func)
+    def wrapper(self, *args):
+        loop = asyncio.get_event_loop()
+        future = loop.create_task(async_func(self, *args[:-1]))
+        future.add_done_callback(log_error)
+
+    return wrapper
 
 '''
 Thread to record video
@@ -61,7 +101,7 @@ def stopVideo():
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self,loop):
         super(MainWindow, self).__init__()
         uic.loadUi("ui_files/MainWindow.ui", self)
         self.setWindowTitle("Manual Remote Control")
@@ -69,22 +109,78 @@ class MainWindow(QtWidgets.QMainWindow):
         self.lbl_image.setPixmap(QPixmap("ui_files/black.png"))
         self.lbl_image.setScaledContents(True)
 
+        #setup hostlink
+        self.loop = loop
+
+
         # signals & slots
         self.btn_start.clicked.connect(startVideo)
         self.btn_stop.clicked.connect(stopVideo)
         self.combo_indice_camera.addItems(Utils.list_ports())
 
+        Utils.setup_serial(self)
+        self.btn_connect.clicked.connect(self.open_port)
+        self.btn_disconnect.clicked.connect(self.close_port)
+
         # show interface
         self.show()
+
+    @slot_coroutine
+    async def send_message(self):
+        msg = self.output_field.text()
+        await self.port.send(msg)
+        #msg = msg.rstrip('\r')
+        #self.response_field.appendPlainText(f"PC -> {msg}")
+
+    def recv_message(self, msg):
+        msg = msg.rstrip('\r')
+        #self.response_field.appendPlainText(f"PLC -> {msg}")
+
+    @slot_coroutine
+    async def open_port(self):
+        port = self.combo_comport.currentData()
+        data_bits = self.combo_data_bits.currentData()
+        stop_bits = self.combo_stop_bits.currentData()
+        parity = self.combo_parity.currentData()
+        bauds = self.combo_baudrate.currentData()
+
+        self.serial_coro = create_serial_connection(self.loop,
+                                                    Utils.Output,
+                                                    url=port[0],
+                                                    bytesize=data_bits,
+                                                    stopbits=stop_bits,
+                                                    parity=parity,
+                                                    baudrate=bauds)
+
+        self.transport, self.port = await self.serial_coro
+
+        self.port.set_recv_callback(self.recv_message)
+
+        self.btn_connect.setDisabled(True)
+        self.btn_disconnect.setDisabled(False)
+        #self.send_button.setDisabled(False)
+
+    def close_port(self):
+        self.loop.stop()
+        self.btn_connect.setDisabled(False)
+        self.btn_disconnect.setDisabled(True)
+        #self.send_button.setDisabled(True)
+
 
 
 if __name__ == "__main__":
     print('Loading user interface...')
     QApp = QtWidgets.QApplication(sys.argv)
     QApp.setStyle("Fusion")
-    window = MainWindow()
+
+    loop = QEventLoop(QApp)
+
+
+    window = MainWindow(loop)
     window.show()
 
+    with loop:
+        loop.run_forever()
     # creates global threads variables to be used through whole file
     videoThread = None
     print("All initialized")
